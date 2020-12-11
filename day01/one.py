baf386dad51e0264ee555c817770f037f4625e0c
ovlu <50896315+ovlu@users.noreply.github.com>
2020-12-11 11:12:58 +0800

    1

--- day01/one.py
+++ day01/one.py
@@ -1,2 +1,14 @@
 # 引入包
 import requests
+import requests.json
+
+url='请求地址'
+# 传参
+data={
+    '值':'zhi',
+    '值2':'zhi2'
+}#字典，转换成json字符串
+data=json.dumps()
+res=requests.request(method="post",url=url,data=data)
+# 获取响应值
+print(res.text)
