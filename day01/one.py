e3bb5e802acb9863c509f020e24123701a8cfa13
ovlu <50896315+ovlu@users.noreply.github.com>
2020-12-11 17:18:22 +0800

    单元测试

--- day01/one.py
+++ day01/one.py
@@ -1,14 +1,35 @@
 # 引入包
 import requests
-import requests.json
+import json
+# 单元测试框架
+import unittest
+import allure_commons
 
-url='请求地址'
-# 传参
-data={
-    '值':'zhi',
-    '值2':'zhi2'
-}#字典，转换成json字符串
-data=json.dumps()
-res=requests.request(method="post",url=url,data=data)
-# 获取响应值
-print(res.text)
+class Test(unittest.TestCase):
+    # 类里面的函数，第一个参数为self
+    def test(self):
+        '''请求的用例'''
+        # 设置请求地址
+        url = 'http://api.tianapi.com/txapi/xhzd/index'
+        data = {
+            'key': '2328169cc2883a60816189e5492f02dd',
+            'word': 'wo',
+            'type': '1'
+        }
+        # 定制头
+        headers = {
+            "content-type": 'application/json'
+        }
+        # 发送请求
+        res = requests.post(url=url, data=data)
+        print(res.text)
+        print(type(res.text))
+        print(type(res.content))
+        # 转换为字典
+        respone=json.loads(res.text)
+        msg=respone['msg']
+        # 设置断言
+        self.assertEqual(msg,"success")
+
+if __name__ == '__main__':
+    unittest.main()
